859d05cc25a85f9aea718d3238c4118a488ca866
ngoduc <ngoducnk@gmail.com>
2019-11-01 19:39:24 +0700

    su dung ham date

---
+++ thoigianhethong/src/SystemTime.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class SystemTime{
+    public static void main(String[] args) {
+        Date present = new Date();
+        System.out.println("thoi gian hien tai la: "+present);
+    }
+}
